f3af32e045d22487bafd1e2e7c42262f84c72638
Gustavo Jeuken <gjeuken@gmail.com>
2024-11-28 16:26:54 +0100

    sc diff expression data

--- single_cell_data.py
+++ single_cell_data.py
@@ -6,6 +6,7 @@ from joblib import Parallel, delayed, cpu_count
 from src.util.qvalue import qvalues
 
 
+
 def anova_proc_scipy(data_groupby, gene_name):
     f, p = f_oneway(*data_groupby[gene_name].apply(list))
     return (gene_name, p, f)
@@ -33,28 +34,52 @@ def log_fold_change(data, label, n_jobs):
     return result_df
 
 
-study = 'E-GEOD-76312'
+def diff_proc(adata, i, label):
+    exp_df = pd.DataFrame(adata.X.getcol(i).todense())
+    exp_df['label'] = label.values
+    exp_groupby = exp_df.groupby('label', observed=True)
+    f, p = f_oneway(*exp_groupby[0].apply(list))
+    if i%100 == 0:
+        print(i)
+    return (i, p, f)
 
-adata = sc.datasets.ebi_expression_atlas(study)
-
-# Basic filtering
-sc.pp.filter_cells(adata, min_genes=200)
-sc.pp.filter_genes(adata, min_cells=3)
-
-# Library size correction
-sc.pp.normalize_total(adata, target_sum=1e4)
-
-# Logarithmize the data
-sc.pp.log1p(adata)
+def diff_par(adata, label, n_jobs):
+    n_genes = adata.X.shape[1]
+    result = Parallel(n_jobs=n_jobs)(delayed(diff_proc)(adata, i, label) for i in range(n_genes))
+    result_df = pd.DataFrame(result, columns=['gene_i', 'p', 'f']).set_index('gene_i')
+    return result_df
 
 
-metadata = adata.obs
-adata = adata.to_df()
+studies = pd.read_csv('./data/singlecell_kegg/sc_kegg_target_disease.tsv', sep = '\t', index_col = 0)
 
-label = metadata['Factor Value[disease]']
+# study = 'E-GEOD-76312'
+# study = studies.index[15]
 
-results = anova_genes_scipy(adata, label, cpu_count()-1)
-results = qvalues(results)
+for study in studies.index:
+    print(study)
 
-logfc = log_fold_change(adata, label, 10)
-results['logfc'] = logfc['logfc']
+    adata = sc.datasets.ebi_expression_atlas(study)
+
+    # Basic filtering
+    sc.pp.filter_cells(adata, min_genes=200)
+    sc.pp.filter_genes(adata, min_cells=3)
+
+    # Library size correction
+    sc.pp.normalize_total(adata, target_sum=1e4)
+
+    # Logarithmize the data
+    sc.pp.log1p(adata)
+
+    metadata = adata.obs
+    # adata = adata.to_df()
+
+    label = metadata['Factor Value[disease]']
+
+    # results = anova_genes_scipy(adata, label, cpu_count()-1)
+    results = diff_par(adata, label, cpu_count()-1)
+    results = qvalues(results)
+    results['gene_name'] = adata.var.iloc[results.index].index
+    results = results.set_index('gene_name')
+
+    results.to_csv(study + '_result.csv')
+
